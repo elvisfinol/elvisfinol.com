8fdce4b8ba94f91bb267f7b7686c9c9f375dbe53
Elvis Finol <elvisfinol@MacBook-Pro-de-Elvis.local>
2024-06-05 01:33:51 +0200

    Cambio

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,22 @@
 #!/bin/zsh
 
+# Prompt user to enter commit message
 vared -p "Enter commit message: " -c message
 
-printf "\e[33m\nBuuilding project...\e[39m\n"
+# Display message indicating project is being built
+printf "\e[33m\nBuilding project...\e[39m\n"
 
+# Display message indicating pushing to repository
 printf "\e[33m\nPushing to elvis.finol.github.io repository...\e[39m\n"
 
+# Add all changes to git
 git add .
 
+# Commit changes with the entered message
 git commit -m "$message"
 
+# Push changes to the main branch of origin
 git push origin main
 
+# Display message indicating successful deployment
 printf "\e[33m\nSuccessfully deployed!\e[39m\n"
