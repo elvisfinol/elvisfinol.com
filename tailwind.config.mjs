7ba90aca19b7a5fc3aff7e3aed41b0f2ef7a48be
Elvis Finol <elvisfinol@MacBook-Pro-de-Elvis.local>
2024-06-28 00:49:02 +0200

    cambios home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
 	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
-	},
+		extend: {
+			animation: {
+				border: 'border 6s ease infinite',
+			  },
+			  keyframes: {
+				border: {
+				  '0%, 100%': { backgroundPosition: '0% 50%' },
+				  '50%': { backgroundPosition: '100% 50%' },
+				},
+			},
+		  },
+		},
 	plugins: [require("@tailwindcss/typography")],
 };
