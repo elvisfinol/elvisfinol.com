822da40192f12bccba9a5e91f5d5d0d6ebded90d
e.finol7@gmail.com <e.finol7@gmail.com>
2024-11-14 12:38:10 +0100

    cambios

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,18 +3,21 @@ export default {
 	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {
-			animation: {
-				border: 'border 6s ease infinite',
-			  },
-			  keyframes: {
-				border: {
-				  '0%, 100%': { backgroundPosition: '0% 50%' },
-				  '50%': { backgroundPosition: '100% 50%' },
-				},
-			},
+	  extend: {
+		fontFamily: {
+		  roboto: ['Roboto', 'sans-serif'],
+		  indie: ['Indie Flower', 'cursive'],
+		},
+		animation: {
+		  border: 'border 6s ease infinite',
+		},
+		keyframes: {
+		  border: {
+			'0%, 100%': { backgroundPosition: '0% 50%' },
+			'50%': { backgroundPosition: '100% 50%' },
 		  },
 		},
+	  },
+	},
 	plugins: [require("@tailwindcss/typography")],
-};
-
+  };
